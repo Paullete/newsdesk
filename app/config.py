167ce7ef8f6d7f84c9847f7b1fcc4a3d47557e3e
paullete <paulettenereah3@gmail.com>
2022-05-02 17:05:05 +0300

    init

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ class Config:
     General configuration parent class
     """
     HEADLINES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    SOURCES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
 
 
 class ProdConfig(Config):
