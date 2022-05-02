61b57c888d086eaf444453b5da4475faf95a4238
paullete <paulettenereah3@gmail.com>
2022-05-02 17:02:00 +0300

    headlines

--- app/models/headlines.py
+++ app/models/headlines.py
@@ -1,10 +1,18 @@
 class Headlines:
     
-    def __init__(self, author, title, description, url, urlToImage, publishedAt):
+    def __init__(self, source, author, title, description, url, urlToImage, publishedAt):
+        self.source = source
         self.author = author
         self.title = title
         self.description = description
         self.url = url
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
-     
+
+
+class Sources:
+
+    def __init__(self, name, description, url):
+        self.name = name
+        self.description = description
+        self.url = url
