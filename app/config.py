d312810fc4d5d5668c40c2ac9a10efc1699ada74
paullete <paulettenereah3@gmail.com>
2022-05-03 18:20:57 +0300

    readme

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class Config:
     """
     HEADLINES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     SOURCES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    # SECRET_KEY = ('SECRET_KEY')
 
 
 class ProdConfig(Config):
